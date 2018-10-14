019862f538d8bb24f8bd8d0935e659ce62fe1cf1
vvaleron <valeriy.shkolniy@gmail.com>
2018-10-14 10:54:28 +0300

    fix model User.js

--- models/User.js
+++ models/User.js
@@ -3,8 +3,14 @@ var keystone = require('keystone');
 var User = new keystone.List('User');
 
 User.add({
-  displayName: { type: String },
-  password: { type: keystone.Field.Types.Password },
+  displayName: {type: String },
   email: { type: keystone.Field.Types.Email, unique: true },
+  password: { type: keystone.Field.Types.Password },
 });
 
+User.schema.virtual('canAccessKeystone').get(function () {
+  return true;
+});
+
+User.defaultColumns = 'id, displayName, email';
+User.register();
